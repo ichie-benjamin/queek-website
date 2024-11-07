fbddd00258386e02463f1c46b15d15f32d50c487
benny <benjaminchukwudi0@gmail.com>
2024-11-07 09:49:00 +0100

    updated

--- components/DownloadApp.tsx
+++ components/DownloadApp.tsx
@@ -1,26 +1,44 @@
 
 import React from 'react'
 import Button from "@/components/Button";
+import ExternalLinkButton from "@/components/ExternalLinkButton";
+import {siteConfig} from "@/config/site";
 
 const DownloadApp = () => {
   return (
       <div className="flex mt-6 w-full flex-col gap-3 whitespace-nowrap xl:flex-row">
 
-        <Button
-            type="button"
-            title="Play Store"
-            icon="/images/android.svg"
-            variant="btn_green"
-            full
-        />
+          <ExternalLinkButton
+              href={ siteConfig.ios }
+              type="button"
+              title="App Store"
+              icon="/images/apple.svg"
+              variant="btn_green"
+          />
 
-        <Button
-            type="button"
-            title="App Store"
-            icon="/images/apple.svg"
-            variant="btn_dark_green_outline"
-            full
-        />
+          <ExternalLinkButton
+              href={ siteConfig.android }
+              type="button"
+              title="Play Store"
+              icon="/images/android.svg"
+              variant="btn_dark_green_outline"
+          />
+
+        {/*<Button*/}
+        {/*    type="button"*/}
+        {/*    title="Play Store"*/}
+        {/*    icon="/images/android.svg"*/}
+        {/*    variant="btn_green"*/}
+        {/*    full*/}
+        {/*/>*/}
+
+        {/*<Button*/}
+        {/*    type="button"*/}
+        {/*    title="App Store"*/}
+        {/*    icon="/images/apple.svg"*/}
+        {/*    variant="btn_dark_green_outline"*/}
+        {/*    full*/}
+        {/*/>*/}
 
       </div>
   )
--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,8 @@
 export const siteConfig = {
     name: "QueekApp",
     author: "QueekApp",
+    ios:"https://apps.apple.com/app/queek-the-everything-app/id6692619520",
+    android:"https://play.google.com/store/apps/details?id=com.queekng.app",
     url: "https://queek.com.ng",
     vendor_signup: "https://app.queek.com.ng/vendor-signup",
     description: "Queekapp Services",
